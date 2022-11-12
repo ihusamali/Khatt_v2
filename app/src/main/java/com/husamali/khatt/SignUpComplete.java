f8efcd8feb74017c20a4c2dc4c66eaad18115bc2
East <i190409@nu.edu.pk>
2022-11-13 02:13:22 +0500

    Changes

---
+++ app/src/main/java/com/husamali/khatt/SignUpComplete.java
@@ -0,0 +1,14 @@
+package com.husamali.khatt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignUpComplete extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_complete);
+    }
+}
