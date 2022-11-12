36da27a314804be3c7a0678ac3b46ceb504ce05f
East <i190409@nu.edu.pk>
2022-11-13 01:17:32 +0500

    Changes

--- app/src/main/java/com/husamali/khatt/SignUp.java
+++ app/src/main/java/com/husamali/khatt/SignUp.java
@@ -9,5 +9,7 @@ public class SignUp extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_up);
+
     }
 }
