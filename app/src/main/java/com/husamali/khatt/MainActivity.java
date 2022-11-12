05394c3690a1160a4d73e1c1636351edbbb38553
saad-khan-rind <khan.saad45678900@gmail.com>
2022-11-12 23:39:25 +0500

    splash and home

--- app/src/main/java/com/husamali/khatt/MainActivity.java
+++ app/src/main/java/com/husamali/khatt/MainActivity.java
@@ -2,7 +2,9 @@ package com.husamali.khatt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -12,16 +14,18 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     ImageView khatt;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         khatt = findViewById(R.id.khatt);
-
-
-//        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
-//        khatt.startAnimation(animation1);
-
+        handler=new Handler();
+        handler.postDelayed(() -> {
+            Intent intent=new Intent(MainActivity.this, Home.class);
+            startActivity(intent);
+            finish();
+        },1000);
     }
 }
