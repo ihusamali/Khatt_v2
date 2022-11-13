53c265625dfe112119eb531b6b51d71a025e9012
East <i190409@nu.edu.pk>
2022-11-13 18:25:03 +0500

    Changes

--- app/src/main/java/com/husamali/khatt/SignUpComplete.java
+++ app/src/main/java/com/husamali/khatt/SignUpComplete.java
@@ -2,13 +2,28 @@ package com.husamali.khatt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUpComplete extends AppCompatActivity {
+
+    Button finish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up_complete);
+
+        finish = findViewById(R.id.finish);
+
+        finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(), Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
