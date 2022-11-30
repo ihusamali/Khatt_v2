1766e87df5a58fd3d39a5f141c7cb9fddfe159ff
saad-khan-rind <khan.saad45678900@gmail.com>
2022-12-01 01:09:49 +0500

    OTP

--- app/src/main/java/com/husamali/khatt/Otp.java
+++ app/src/main/java/com/husamali/khatt/Otp.java
@@ -1,15 +1,40 @@
 package com.husamali.khatt;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class Otp extends AppCompatActivity {
 
     Button next;
+    EditText otp;
+    String number;
+    FirebaseAuth mAuth;
+    String codeGot;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +42,70 @@ public class Otp extends AppCompatActivity {
         setContentView(R.layout.activity_otp);
 
         next = findViewById(R.id.next);
+        otp = findViewById(R.id.otp);
+        number = getIntent().getStringExtra("number");
+        mAuth = FirebaseAuth.getInstance();
+        initial();
+
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), SignUpComplete.class);
-                startActivity(intent);
+                if(otp.getText()!=null) {
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeGot,otp.getText().toString());
+                    signInWithPhoneAuthCredential(credential);
+                }
             }
         });
+    }
+
+    private void initial() {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(number)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(Otp.this)                 // Activity (for callback binding)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                super.onCodeSent(s, forceResendingToken);
+                                codeGot = s;
+                            }
+
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                final String code = phoneAuthCredential.getSmsCode();
+                                if(code != null)
+                                {
+                                    Toast.makeText(getApplicationContext(),"1 1 1",Toast.LENGTH_SHORT).show();
+                                    signInWithPhoneAuthCredential(phoneAuthCredential);
+
+                                }
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(getApplicationContext(),"other fail "+e.getMessage(),Toast.LENGTH_LONG).show();
+                            }})          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),"1 1 1 1 1 1",Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(getApplicationContext(), SignUpComplete.class);
+                            startActivity(intent);
+
+                        } else {
+                            Toast.makeText(getApplicationContext(),"other fail ",Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/husamali/khatt/SignUp.java
+++ app/src/main/java/com/husamali/khatt/SignUp.java
@@ -1,30 +1,61 @@
 package com.husamali.khatt;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignUp extends AppCompatActivity {
 
     Button next;
+    EditText number;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         next = findViewById(R.id.next);
+        number = findViewById(R.id.numberSignIn);
+        mAuth = FirebaseAuth.getInstance();
+
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(), Otp.class);
-                startActivity(intent);
+                if(number.getText()!=null) {
+
+                    Intent intent = new Intent(getApplicationContext(), Otp.class);
+                    intent.putExtra("number", number.getText().toString());
+                    startActivity(intent);
+                }
             }
         });
 
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(mAuth.getCurrentUser()!=null){
+            Intent intent = new Intent(getApplicationContext(), Home.class);
+            startActivity(intent);
+        }
 
     }
 }
